566650c13eb8bf2ab312ecfbc48f0caf432544b9
TWT SnoW <nicolaslopesaquino@gmail.com>
2019-11-16 14:40:34 -0300

    Capturing button event and saving photo

---
+++ tirarFoto.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import cv2
+
+GPIO.setmode(GPIO.BOARD)
+
+#Definindo o pino 18 como entrada
+pinButao = 18
+GPIO.setup(pinButao, GPIO.IN)
+
+#Iniciando WebCam
+video = cv2.VideoCapture(0)
+
+def capturarEvento():
+    #Pegando Status e Frame do vídeo
+    ret, frame = video.read()
+    
+    #Verificando estado do botão
+    if GPIO.input(pinButao):
+        cv2.imwrite("foi.jpg",frame)
+        return True #Caso verdadeiro, salva imagem e retorna True
+
+    else:
+        return False #Caso falso, retorna falso
